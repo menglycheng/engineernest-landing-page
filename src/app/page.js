b85455a80fbd07db126b575140609b99ff34342f
menglycheng <menglycheng2@gmail.com>
2023-08-02 15:53:51 +0700

    fixed scroll bar

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import { HeroSection } from "@/components/HeroSection";
 import { OurTeam } from "@/components/OurTeam";
 import { OurVision } from "@/components/OurVision";
 import { Parther } from "@/components/Parther";
-import Image from "next/image";
 
 export default function Home() {
   return (
